7758789fd6db5ebaedb2c763e64711e72d0407d8
tanmally <tm@beyondbytes.co.in>
2014-01-20 23:22:53 +0530

    Added more api to daas client

--- daas-client/src/main/java/com/bbytes/daas/client/DaasManagementClient.java
+++ daas-client/src/main/java/com/bbytes/daas/client/DaasManagementClient.java
@@ -5,6 +5,8 @@ import java.util.List;
 import java.util.concurrent.Future;
 
 import com.bbytes.daas.domain.Account;
+import com.bbytes.daas.domain.Application;
+import com.bbytes.daas.domain.DaasUser;
 import com.google.gson.reflect.TypeToken;
 import com.ning.http.client.Response;
 
@@ -49,10 +51,11 @@ public class DaasManagementClient extends DaasClient {
 	}
 
 	public Account createAccount(String accName) throws DaasClientException {
-		
+
 		try {
-			accName = URLEncoder.encode(accName,"UTF-8");
-			String url = baseURL + URLConstants.MANAGEMENT_CONTEXT + String.format(URLConstants.CREATE_ACCOUNT, accName);
+			accName = URLEncoder.encode(accName, "UTF-8");
+			String url = baseURL + URLConstants.MANAGEMENT_CONTEXT
+					+ String.format(URLConstants.CREATE_DELETE_ACCOUNT, accName);
 			Future<Response> f = buildRequest("post", url).execute();
 			Response r = f.get();
 			if (!HttpStatusUtil.isSuccess(r))
@@ -66,17 +69,113 @@ public class DaasManagementClient extends DaasClient {
 		}
 	}
 
+	public boolean deleteAccount(String accName) throws DaasClientException {
+		try {
+			accName = URLEncoder.encode(accName, "UTF-8");
+			String url = baseURL + URLConstants.MANAGEMENT_CONTEXT
+					+ String.format(URLConstants.CREATE_DELETE_ACCOUNT, accName);
+			Future<Response> f = buildRequest("delete", url).execute();
+			Response r = f.get();
+			if (!HttpStatusUtil.isSuccess(r))
+				throw new DaasClientException("Account deletion failed : " + r.getResponseBody());
+
+			return true;
+
+		} catch (Exception e) {
+			throw new DaasClientException(e);
+		}
+	}
+
 	public List<Account> getAccounts() throws DaasClientException {
 		String url = baseURL + URLConstants.MANAGEMENT_CONTEXT + URLConstants.GET_ALL_ACCOUNT;
 		try {
 			Future<Response> f = buildRequest("get", url).execute();
 			Response r = f.get();
 			if (!HttpStatusUtil.isSuccess(r))
-				throw new DaasClientException("Could not fetch all accounts : " +  r.getResponseBody());
+				throw new DaasClientException("Could not fetch all accounts : " + r.getResponseBody());
 
 			List<Account> accounts = gson.fromJson(r.getResponseBody(), new TypeToken<List<Account>>() {
 			}.getType());
 			return accounts;
+
+		} catch (Exception e) {
+			throw new DaasClientException(e);
+		}
+	}
+
+	public Application createApplication(Application application) throws DaasClientException {
+
+		try {
+			String appName = URLEncoder.encode(application.getName(), "UTF-8");
+			String accName = URLEncoder.encode(application.getAccountName(), "UTF-8");
+			String url = baseURL + URLConstants.MANAGEMENT_CONTEXT
+					+ String.format(URLConstants.CREATE_DELETE_APPLICATION, accName, appName);
+			Future<Response> f = buildRequest("post", url).setBody(gson.toJson(application))
+					.setHeader("Content-Type", "application/json").execute();
+			Response r = f.get();
+			if (!HttpStatusUtil.isSuccess(r))
+				throw new DaasClientException("Application creation failed : " + r.getResponseBody());
+
+			application = gson.fromJson(r.getResponseBody(), Application.class);
+			return application;
+
+		} catch (Exception e) {
+			throw new DaasClientException(e);
+		}
+	}
+
+	public boolean deleteApplication(String accName, String appName) throws DaasClientException {
+
+		try {
+			appName = URLEncoder.encode(appName, "UTF-8");
+			String url = baseURL + URLConstants.MANAGEMENT_CONTEXT
+					+ String.format(URLConstants.CREATE_DELETE_APPLICATION, accName, appName);
+			Future<Response> f = buildRequest("delete", url).execute();
+			Response r = f.get();
+			if (!HttpStatusUtil.isSuccess(r))
+				throw new DaasClientException("Application deletion failed : " + r.getResponseBody());
+
+			return true;
+
+		} catch (Exception e) {
+			throw new DaasClientException(e);
+		}
+
+	}
+
+	public List<Application> getApplications(String accName) throws DaasClientException {
+		String url = baseURL + URLConstants.MANAGEMENT_CONTEXT
+				+ String.format(URLConstants.GET_ALL_APPLICATIONS, accName);
+		;
+		try {
+			Future<Response> f = buildRequest("get", url).execute();
+			Response r = f.get();
+			if (!HttpStatusUtil.isSuccess(r))
+				throw new DaasClientException("Could not fetch all applications : " + r.getResponseBody());
+
+			List<Application> applications = gson.fromJson(r.getResponseBody(), new TypeToken<List<Application>>() {
+			}.getType());
+			return applications;
+
+		} catch (Exception e) {
+			throw new DaasClientException(e);
+		}
+	}
+
+	public DaasUser createAccountUser(String accName, DaasUser user) throws DaasClientException {
+
+		try {
+			accName = URLEncoder.encode(accName, "UTF-8");
+			String url = baseURL + URLConstants.MANAGEMENT_CONTEXT
+					+ String.format(URLConstants.CREATE_ACCOUNT_USER, accName);
+			Future<Response> f = buildRequest("post", url).setBody(gson.toJson(user))
+					.setHeader("Content-Type", "application/json").execute();
+			Response r = f.get();
+			if (!HttpStatusUtil.isSuccess(r))
+				throw new DaasClientException("Account user creation failed : " + r.getResponseBody());
+
+			DaasUser accountUser = gson.fromJson(r.getResponseBody(), DaasUser.class);
+			return accountUser;
 
 		} catch (Exception e) {
 			throw new DaasClientException(e);
