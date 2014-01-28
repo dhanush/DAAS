e0f3a93ed8fe10403011b275f2131cd63d48e423
tanmally <tm@beyondbytes.co.in>
2014-01-28 12:55:32 +0530

    Fixed imports

--- daas-client/src/test/java/com/bbytes/daas/client/AnnotationDaasClientCRUDTest.java
+++ daas-client/src/test/java/com/bbytes/daas/client/AnnotationDaasClientCRUDTest.java
@@ -13,10 +13,6 @@
  */
 package com.bbytes.daas.client;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
